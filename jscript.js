ad2513ba501e89d903fc4d8ca9a708721c06e576
Tha33mF <128337832+Tha33mF@users.noreply.github.com>
2025-03-18 21:47:10 -0500

    jscript.js

--- jscript.js
+++ jscript.js
@@ -44,15 +44,15 @@ var password = pwd;
 
 
 const products = [
-    { id: "item1", name: "FC BARCELONA Away 14/15", price: 5000 },
+    { id: "item1", name: "FC BARCELONA Home 14/15", price: 5000 },
     { id: "item2", name: "FC BARCELONA Away 15/16", price: 6000 },
-    { id: "item3", name: "FC BARCELONA Away 16/17", price: 6000 },
+    { id: "item3", name: "FC BARCELONA Third 16/17", price: 6000 },
     { id: "item4", name: "FC BARCELONA Away 16/17", price: 7000 },
-    { id: "item5", name: "FC BARCELONA Away 17/18", price: 6000 },
+    { id: "item5", name: "FC BARCELONA Home 17/18", price: 6000 },
     { id: "item6", name: "FC BARCELONA Away 18/19", price: 5000 },
-    { id: "item7", name: "FC BARCELONA Away 19/20", price: 5000 },
+    { id: "item7", name: "FC BARCELONA Home 19/20", price: 5000 },
     { id: "item8", name: "FC BARCELONA Away 21/22", price: 6500 },
-    { id: "item9", name: "FC BARCELONA Away 22/23", price: 7000 }
+    { id: "item9", name: "PSG MESSI ICONIC Away KIT 22/23", price: 7000 }
 ];
 
 function confirmSelection() {
